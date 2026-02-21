770b6d2ef52b4ee4fed5e7ccaecc7595246737c6
lilyco42 <liuqingffffff@gmail.com>
2026-02-21 10:04:35 +0800

    local build successful

--- tauri-tools/src-tauri/src/lib.rs
+++ tauri-tools/src-tauri/src/lib.rs
@@ -4,7 +4,6 @@ async fn generate() {}
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .manage(ModelState(Mutex::new(None)))
         .invoke_handler(tauri::generate_handler![generate])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
